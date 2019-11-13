ce30b0cf5f6867d643b78499954ce6af1fdb0718
Simona-de <57255935+Simona-de@users.noreply.github.com>
2019-11-13 21:59:06 +0100

    js date

--- src/script.js
+++ src/script.js
@@ -1,14 +1,32 @@
+function formatDate(timespamp) {
+  let date = new date(timespamp);
+  let hours = date.gethours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let mintues = date.getminuts();
+  if (mintues < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = ["sun", "mon", "tue", "wen", "thu", "fri", "sat"];
+  let day = days[now.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
+
 function showTemperature(response) {
-  let temperature = document.querySelector("#temp");
-  temperature.innerHTML = Math.round(response.data.main.temp);
-  let city = document.querySelector("#city");
-  city.innerHTML = response.data.name;
-  let sky = document.querySelector("#sky");
-  sky.innerHTML = response.data.weather[0].description;
-  let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = response.data.main.humidity;
-  let wind = document.querySelector("#wind");
-  wind.innerHTML = response.data.wind.speed;
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let dateElement = document.querySelector("#date");
+
+  let skyElement = document.querySelector("#sky");
+  skyElement.innerHTML = response.data.weather[0].description;
+  let humidityElement = document.querySelector("#humidity");
+  humidityElement.innerHTML = response.data.main.humidity;
+  let windElement = document.querySelector("#wind");
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   console.log(response);
 }
 
