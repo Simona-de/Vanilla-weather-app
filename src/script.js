9fe36c22da2af98b04e7998bce74f969983181d2
Simona-de <57255935+Simona-de@users.noreply.github.com>
2019-11-13 21:13:10 +0100

    added show temperature function

--- src/script.js
+++ src/script.js
@@ -1,1 +1,17 @@
+function showTemperature(response) {
+  let temperature = document.querySelector("#temp");
+  temperature.innerHTML = Math.round(response.data.main.temp);
+  let city = document.querySelector("#city");
+  city.innerHTML = response.data.name;
+  let sky = document.querySelector("#sky");
+  sky.innerHTML = response.data.weather[0].description;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = response.data.main.humidity;
+  let wind = document.querySelector("#wind");
+  wind.innerHTML = response.data.wind.speed;
+  console.log(response);
+}
 
+let apiKey = "71f687cf35794567462cfbc034ffcc9d";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=sydney&units=metric&appid=${apiKey}`;
+axios.get(`${apiUrl}`).then(showTemperature);
