a3f550d41a9d13e1dcb32f284443a7f44fe18517
Simona-de <57255935+Simona-de@users.noreply.github.com>
2019-11-17 12:42:54 +0100

    added CSS to forecast

--- src/script.js
+++ src/script.js
@@ -56,7 +56,7 @@ function showForecast(response) {
   for (let index = 0; index < 6; index++) {
     forecast = response.data.list[index];
     forecastElement.innerHTML += `
-  <div class="col-2">
+  <div class="col-small-2">
   <h3>
   ${formatHours(forecast.dt * 1000)}
   </h3>
